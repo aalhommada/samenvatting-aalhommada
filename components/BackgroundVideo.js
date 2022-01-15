fd431c16c19fd02f31f5d2e238e6a272a77a2912
DESKTOP-IDDMESE\abdul <aalhommada@gmail.com>
2022-01-15 11:01:00 +0100

    update title

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -2,8 +2,8 @@ export default function BackgroundVideo() {
   return (
     <div className="relative flex items-center justify-center h-screen m-0 p-0 overflow-hidden">
       <div className="relative z-30 p-5 text-2xl text-white bg-indigo-800 bg-opacity-60 rounded-xl mx-5">
-        <h2 className="text-5xl font-extrabold">Welkom op de website van SamenVatten.</h2>
-        <p className="text-md">Ik ben Ali en ik doe SamenVatting van een boeken die ik heb gelezen.</p>
+        <h2 className="text-5xl font-extrabold">Welkom op de website van Samenvatten.</h2>
+        <p className="text-md">Ik ben Ali en ik doe Samenvatting van een boeken die ik heb gelezen.</p>
       </div>
       <video
         src="/videos/video-books.mp4"
